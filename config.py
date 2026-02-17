8b072d315f225a5bdf111adf4314cc67acb916c6
IDGS-801-23001542 <83955@alumnos.utleon.edu.mx>
2026-02-16 18:47:20 -0600

    INSERTAR 16/FEBRERO/2026

--- config.py
+++ config.py
@@ -1,11 +1,12 @@
 import os
+
 from sqlalchemy import create_engine
 
 class Config(object):
-    SECRER_KEY = "ClaveSecreta"
+    SECRET_KEY = "ClaveSecreta"
     SESSION_COOKIE_SECURE = False
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://Jeypeeh:root@127.0.0.1/bdidgs801'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@127.0.0.1/bdidgs801'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
